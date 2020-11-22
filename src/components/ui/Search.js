4384ca6f9b969f0c10765fb4bc2c77c7f8d0d904
İbrahim <carbugaibrahim@hotmail.com>
2020-11-22 19:12:48 +0300

    Breaking Bad Cast App

---
+++ src/components/ui/Search.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function Search({getQuery}) {
+  const [text, setText] = useState("");
+
+  const onChange = (q) => {
+      setText(q);
+      getQuery(q);
+  };
+
+  return (
+    <section className="search">
+      <form>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search Characters"
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
+          autoFocus
+        ></input>
+      </form>
+    </section>
+  );
+}
+
+export default Search;
